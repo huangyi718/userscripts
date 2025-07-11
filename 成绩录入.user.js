505bdf4e3fc5698f1c6405524c44be66dcb8c008
huangyi718 <huangyi.cqu@gmail.com>
2025-07-11 11:25:15 +0800

    成绩录入.user.js

--- 成绩录入.user.js
+++ 成绩录入.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Excel 上传按钮（支持 iframe 和动态加载）
 // @namespace    http://tampermonkey.net/
-// @version      2.3  
+// @version      2.4
 // @description  在网页右上角添加 Excel 上传按钮...
 // @author       You
 // @include      *://*.cqwu.edu.cn/*
