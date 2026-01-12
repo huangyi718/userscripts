08549caa82812373af1d806e6d7357f0ed981924
huangyi718 <huangyi.cqu@gmail.com>
2026-01-12 21:59:50 +0800

    Create fif-helper.user.js

---
+++ fif-helper.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         FIF阅卷助手-开源版
+// @namespace    http://tampermonkey.net/
+// @version      5.2
+// @description  A键映射到系统原生的+0按钮，支持全按键自定义。
+// @author       HY
+// @match        *://aimark.fifedu.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-end
+// @updateURL    https://raw.githubusercontent.com/huangyi718/userscripts/main/fif-helper.user.js
+// @downloadURL  https://raw.githubusercontent.com/huangyi718/userscripts/main//fif-helper.user.js
+// ==/UserScript==
